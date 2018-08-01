9a1a811b95770be47e64667a33a47d2462920bf9
Shwe Pyi Soe <doctorlay.sps@gmail.com>
2018-08-01 23:10:12 +0630

    Add files via upload

---
+++ src/main/java/com/example/model/Student.java
@@ -0,0 +1,11 @@
+package com.springboot.demo.model;
+
+public class Student {
+	private int empId;
+	private String name;
+	private String address;
+	private String contactNumber;
+	private String email;
+	private int salary;
+	private String degree;
+}
